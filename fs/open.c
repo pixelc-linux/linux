ebf3e89a5fdf40127224d03a2ea8e599130c0c83
Miklos Szeredi <mszeredi@redhat.com>
2018-06-05 12:28:23 +0200

    vfs: simplify dentry_open()
    dentry_open() can now just call path_open().
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/open.c
+++ fs/open.c
@@ -944,31 +944,12 @@ EXPORT_SYMBOL_GPL(path_open);
 struct file *dentry_open(const struct path *path, int flags,
 			 const struct cred *cred)
 {
-	int error;
-	struct file *f;
-
 	validate_creds(cred);
 
 	/* We must always pass in a valid mount pointer. */
 	BUG_ON(!path->mnt);
 
-	f = get_empty_filp();
-	if (!IS_ERR(f)) {
-		f->f_flags = flags;
-		error = vfs_open(path, f, cred);
-		if (!error) {
-			/* from now on we need fput() to dispose of f */
-			error = open_check_o_direct(f);
-			if (error) {
-				fput(f);
-				f = ERR_PTR(error);
-			}
-		} else { 
-			put_filp(f);
-			f = ERR_PTR(error);
-		}
-	}
-	return f;
+	return path_open(path, flags, d_backing_inode(path->dentry), cred, true);
 }
 EXPORT_SYMBOL(dentry_open);
 
